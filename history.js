f91590016dc0cafdc5da2fca03ccc8f57fb84c9b
sumaiya357 <rahmansumaiya75@gmail.com>
2022-09-15 16:16:45 +0600

    0 is removed

--- history.js
+++ history.js
@@ -4,8 +4,8 @@ function addHistory(questionText, timeTaken, errorCount) {
   const newRow = document.createElement("div");
   newRow.classList.add("card");
 
-  newRow.innerHTML = `
-  <h3>${questionText}</h3>
+  newRow.innerHTML = 
+  ` <h3>${questionText}</h3>
   <div>
   <p>You took: <span class="bold">${timeTaken}</span> seconds</p>
   <p>You made <span class="bold red">${errorCount}</span> mistakes</p>
